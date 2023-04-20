eb0b06930fcea8e877e18e7e889dacbfda57fbb3
Ru Ji <ruji@v1040-wn-rt-a-117-38.campus-dynamic.uwaterloo.ca>
2023-04-20 19:38:36 -0400

    eval

--- examples/openmp/btree.cpp
+++ examples/openmp/btree.cpp
@@ -7,20 +7,20 @@
 #include <stdgpu/iterator.h>
 #include <stdgpu/memory.h>
 #include <stdgpu/platform.h>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 
 
 void
 insert_neighbors(const int* b_input, const stdgpu::index_t n, stdgpu::btree<int> &btree)
 {
-//#pragma omp parallel for
+#pragma omp parallel for
     for (stdgpu::index_t i = 0; i < n; ++i)
     {
         int num = b_input[i];
-        int num_neighborhood[3] = { num + 2, num + 3, num + 4};
-        for (int num_neighbor: num_neighborhood){
-            // dump everythin into the tree
-            btree.insert(num_neighbor);
-        }
+        btree.insert(num);
+
     }
 }
 
@@ -28,15 +28,12 @@ insert_neighbors(const int* b_input, const stdgpu::index_t n, stdgpu::btree<int>
 void
 remove_neighbors(const int* b_input, const stdgpu::index_t n, stdgpu::btree<int> &btree)
 {
-//#pragma omp parallel for
+#pragma omp parallel for
     for (stdgpu::index_t i = 0; i < n; ++i)
     {
         int num = b_input[i];
-        int num_neighborhood[3] = { num + 2, num + 3, num + 4};
-        for (int num_neighbor: num_neighborhood){
-            // dump everythin into the tree
-            btree.erase(num_neighbor);
-        }
+        btree.erase(num);
+
     }
 }
 
@@ -44,34 +41,45 @@ remove_neighbors(const int* b_input, const stdgpu::index_t n, stdgpu::btree<int>
 int
 main()
 {
-    const stdgpu::index_t n = 100;
-    int * b_input = createDeviceArray<int>(n);
-    // I don't understand why every number is contained 3 times, but I set it as this for now.
-    stdgpu::btree<int> btree = stdgpu::btree<int>::createDeviceObject(3 * n);
-    // doc can be found here
-    // https://docs.nvidia.com/cuda/thrust/
+    // open the file
+    std::ofstream myfile("btree.csv");
+    std::vector<int> v = {100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200, 102400};
+    for(int i= 0; i < v.size(); i++){
+        const stdgpu::index_t n = v[i];
+        int * b_input = createDeviceArray<int>(n);
+        // I don't understand why every number is contained 3 times, but I set it as this for now.
+        stdgpu::btree<int> btree = stdgpu::btree<int>::createDeviceObject(n*3+3);
+        // doc can be found here
+        // https://docs.nvidia.com/cuda/thrust/
 
-    thrust::sequence(stdgpu::device_begin(b_input), stdgpu::device_end(b_input), 1);
+        thrust::sequence(stdgpu::device_begin(b_input), stdgpu::device_end(b_input), 1);
+        auto start = std::chrono::system_clock::now();
+        insert_neighbors(b_input, n, btree);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        myfile << v[i] << "," << elapsed_seconds.count() << "," << "insert" << "\n";
 
-    insert_neighbors(b_input, n, btree);
+        auto range_btree = btree.device_range();
+        long long sum = thrust::reduce(range_btree.begin(), range_btree.end(), 0, thrust::plus<int>());
+        long long sum_closed_form = (long long)n * ((long long )n + 1) / 2;
 
-    auto range_btree = btree.device_range();
-    int sum = thrust::reduce(range_btree.begin(), range_btree.end(), 0, thrust::plus<int>());
-    const int sum_closed_form = (3 + n + 4) * (n + 2) / 2;
+        std::cout << "The set of duplicated numbers contains " << btree.size() << " elements (" << n
+                  << " expected) and the computed sum is " << sum << " (" << sum_closed_form << " expected)" << std::endl;
 
-    std::cout << "The set of duplicated numbers contains " << btree.size() << " elements (" << n + 2
-              << " expected) and the computed sum is " << sum << " (" << sum_closed_form << " expected)" << std::endl;
+        start = std::chrono::system_clock::now();
+        remove_neighbors(b_input, n, btree);
+        end = std::chrono::system_clock::now();
+        range_btree = btree.device_range();
+        elapsed_seconds = end - start;
+        myfile << v[i] << "," << elapsed_seconds.count() << "," << "remove" << "\n";
+        sum = thrust::reduce(range_btree.begin(), range_btree.end(), 0, thrust::plus<int>());
+        const int sum_closed_form2 = 0;
 
-    remove_neighbors(b_input, n, btree);
-    range_btree = btree.device_range();
-    sum = thrust::reduce(range_btree.begin(), range_btree.end(), 0, thrust::plus<int>());
-    const int sum_closed_form2 = 0;
+        std::cout << "The set of duplicated numbers contains " << btree.size() << " elements (" << 0
+                  << " expected) and the computed sum is " << sum << " (" << sum_closed_form2 << " expected)" << std::endl;
 
-    std::cout << "The set of duplicated numbers contains " << btree.size() << " elements (" << 0
-              << " expected) and the computed sum is " << sum << " (" << sum_closed_form2 << " expected)" << std::endl;
-
-
-
-    destroyDeviceArray<int>(b_input);
-    stdgpu::btree<int>::destroyDeviceObject(btree);
+        destroyDeviceArray<int>(b_input);
+        stdgpu::btree<int>::destroyDeviceObject(btree);
+    }
+    myfile.close();
 }
--- examples/openmp/unordered_set.cpp
+++ examples/openmp/unordered_set.cpp
@@ -22,13 +22,15 @@
 #include <stdgpu/memory.h>          // createDeviceArray, destroyDeviceArray
 #include <stdgpu/platform.h>        // STDGPU_HOST_DEVICE
 #include <stdgpu/unordered_set.cuh> // stdgpu::unordered_set
-
+#include <fstream>
+#include <chrono>
+#include <ctime>
 struct is_odd
 {
     STDGPU_HOST_DEVICE bool
     operator()(const int x) const
     {
-        return x % 2 == 1;
+        return true;
     }
 };
 
@@ -39,15 +41,19 @@ insert_neighbors(const int* d_result, const stdgpu::index_t n, stdgpu::unordered
     for (stdgpu::index_t i = 0; i < n; ++i)
     {
         int num = d_result[i];
-        int num_neighborhood[3] = { num - 1, num, num + 1 };
-
-        for (int num_neighbor : num_neighborhood)
-        {
-            set.insert(num_neighbor);
-        }
+        set.insert(num);
     }
 }
 
+void
+remove_neighbors(const int* d_result, const stdgpu::index_t n, stdgpu::unordered_set<int> &set )
+{
+#pragma omp parallel for
+    for(stdgpu::index_t i = 0; i < n; ++i){
+        int num = d_result[i];
+        set.erase(num);
+    }
+}
 int
 main()
 {
@@ -56,37 +62,48 @@ main()
     // -------------------
     // This example demonstrates how stdgpu::unordered_set is used to compute a duplicate-free set of numbers.
     //
+    std::ofstream myfile("./unordered_set.csv");
+    std::vector<int> v = {100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200, 102400};
+    for(int i=0; i < v.size(); i++)
+    {
+        const stdgpu::index_t n = v[i];
+        int* d_input = createDeviceArray<int>(n);
+        stdgpu::unordered_set<int> set = stdgpu::unordered_set<int>::createDeviceObject(n);
+        thrust::sequence(stdgpu::device_begin(d_input), stdgpu::device_end(d_input), 1);
 
-    const stdgpu::index_t n = 100;
+        // d_input : 1, 2, 3, ..., 100
 
-    int* d_input = createDeviceArray<int>(n);
-    int* d_result = createDeviceArray<int>(n / 2);
-    stdgpu::unordered_set<int> set = stdgpu::unordered_set<int>::createDeviceObject(n);
+        // d_result : 1, 3, 5, ..., 99
+        auto start = std::chrono::system_clock::now();
+        insert_neighbors(d_input, n, set);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        myfile << v[i] << "," << elapsed_seconds.count() << "," << "insert" << "\n";
+        // set : 0, 1, 2, 3, ..., 100
 
-    thrust::sequence(stdgpu::device_begin(d_input), stdgpu::device_end(d_input), 1);
+        auto range_set = set.device_range();
+        long long sum = thrust::reduce(range_set.begin(), range_set.end(), 0, thrust::plus<int64_t>());
 
-    // d_input : 1, 2, 3, ..., 100
+        long long  sum_closed_form = (long long)n * ((long long )n + 1) / 2;
 
-    thrust::copy_if(stdgpu::device_cbegin(d_input),
-                    stdgpu::device_cend(d_input),
-                    stdgpu::device_begin(d_result),
-                    is_odd());
+        std::cout << "The duplicate-free set of numbers contains " << set.size() << " elements (" << n
+                  << " expected) and the computed sum is " << sum << " (" << sum_closed_form << " expected)" << std::endl;
 
-    // d_result : 1, 3, 5, ..., 99
+        start = std::chrono::system_clock::now();
+        remove_neighbors(d_input, n, set);
+        end = std::chrono::system_clock::now();
+        range_set = set.device_range();
+        elapsed_seconds = end-start;
+        myfile << v[i] << "," << elapsed_seconds.count() << "," << "remove" << "\n";
+        sum = thrust::reduce(range_set.begin(), range_set.end(), 0, thrust::plus<int>());
 
-    insert_neighbors(d_result, n / 2, set);
+        sum_closed_form = n * (n + 1) / 2;
 
-    // set : 0, 1, 2, 3, ..., 100
+        std::cout << "The duplicate-free set of numbers contains " << set.size() << " elements (" << 0
+                  << " expected) and the computed sum is " << sum << " (" << 0<< " expected)" << std::endl;
 
-    auto range_set = set.device_range();
-    int sum = thrust::reduce(range_set.begin(), range_set.end(), 0, thrust::plus<int>());
 
-    const int sum_closed_form = n * (n + 1) / 2;
-
-    std::cout << "The duplicate-free set of numbers contains " << set.size() << " elements (" << n + 1
-              << " expected) and the computed sum is " << sum << " (" << sum_closed_form << " expected)" << std::endl;
-
-    destroyDeviceArray<int>(d_input);
-    destroyDeviceArray<int>(d_result);
-    stdgpu::unordered_set<int>::destroyDeviceObject(set);
+        destroyDeviceArray<int>(d_input);
+        stdgpu::unordered_set<int>::destroyDeviceObject(set);}
+    myfile.close();
 }
